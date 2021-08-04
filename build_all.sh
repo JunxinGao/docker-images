40e57ee578b79dce020de9aa7697c34f96ce2ffb
junxin.gao <junxin.gao@outlook.com>
2021-08-04 18:41:45 +0800

    add jupyter

--- build_all.sh
+++ build_all.sh
@@ -3,12 +3,15 @@ source common.sh
 set -e
 cd $(dirname "$0") #make sure we are in the root of the repo
 
-for d in conda; do
-    cd $d
-    echo "building $d"
-    ../build.sh
-    if [ $? == 0 ]; then
-      ../push.sh
+for d in `ls .`; do
+    if [ -f $d/Dockerfile ]; then
+      cd $d
+      echo "building $d"
+      ../build.sh
+      if [ $? == 0 ]; then
+        echo ".."
+        #../push.sh
+      fi
+      cd ..
     fi
-    cd ..
 done
